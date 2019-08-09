5272e7b50c858b45fa7e335d134916f1c70fd2db
pphu <pphu@tencent.com>
2019-08-09 11:32:09 +0800

    publish自动提交

---
+++ src/helper/timer.js
@@ -0,0 +1,24 @@
+const timeoutArr = []
+const intervalArr = []
+
+export function setTimeout() {
+  const timer = window.setTimeout(...arguments)
+  timeoutArr.push(timer)
+  return timer
+}
+
+export function setInterval() {
+  const timer = window.setInterval(...arguments)
+  intervalArr.push(timer)
+  return timer
+}
+
+export function destory() {
+  timeoutArr.forEach(item => clearTimeout(item))
+  intervalArr.forEach(item => clearInterval(item))
+}
+
+export function logTimer() {
+  console.log('timeoutArr', timeoutArr)
+  console.log('intervalArr', intervalArr)
+}
